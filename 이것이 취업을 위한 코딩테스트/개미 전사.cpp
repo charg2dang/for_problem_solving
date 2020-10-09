439d27b96e176b0b542ee39273ecb2f06ac54b48
charg2dang <55304275+charg2dang@users.noreply.github.com>
2020-10-09 14:19:17 +0900

    DP 개미전사.

---
+++ 이것이 취업을 위한 코딩테스트/개미 전사.cpp
@@ -0,0 +1,32 @@
+int solution()
+{
+    int number;
+    cin >> number;
+
+    vector<int> ns;
+    ns.reserve(number);
+    for (int n{}; n < number; ++n)
+    {
+        int a;
+        cin >> a;
+        ns.emplace_back(a);
+    }
+
+    int dp[100]{};
+
+    dp[0] = ns[0];
+    dp[1] = max(ns[0], ns[1]);
+    for (int n{ 2 }; n < number; ++n)
+    {
+        dp[n] = max(dp[n - 1], dp[n - 2] + ns[n]);
+    }
+
+    return dp[number - 1];
+}
+
+
+int main()
+{
+    cout << solution();
+    return 0;
+}
