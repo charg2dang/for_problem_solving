7a5bccc9e5915b47f4261158a707d1807602d2ab
charg2dang <55304275+charg2dang@users.noreply.github.com>
2020-10-11 00:18:51 +0900

    Update 다익스트라 개선.cpp

--- 이것이 취업을 위한 코딩테스트/다익스트라 개선.cpp
+++ 이것이 취업을 위한 코딩테스트/다익스트라 개선.cpp
@@ -8,15 +8,15 @@ constexpr int inf = int(1e9);
 
 int node_num, edge_num; // 노드와 간선의 수.
 int start_node; // 시작 노드
-vector<pair<int, int>> graph[100'001]; // 각 노드에 연결된 노드에 대한 정보 배열
-int distance[100'001]{};    // 최단 거리 테이블
+vector<pair<int, int>> graph[100'001]; // 각 노드에 연결된 노드에 대한 정보 배열   first 다른 노드, 비용
+int distances[100'001]{};    // 최단 거리 테이블
 
 void dijkstra(int start_node)
 {
     // 시작 노드에 대한 초기화
-    priority_queue<pair<int, int>> pq; // 비용 평가를 위해  < 비용, 다른 노드> 순으로 삽입.
+    priority_queue<pair<int, int>> pq; // 비용 평가를 위해  < 비용, 다른 노드> 순으로 삽입. 기본 max heap
     pq.emplace(make_pair(0, start_node));
-    ::distance[start_node] = 0;
+    ::distances[start_node] = 0;
 
     while (pq.empty() == false) // 큐가 비어 있지 않다면?
     {
@@ -27,22 +27,24 @@ void dijkstra(int start_node)
         pq.pop();
 
         // 현재 노드가 이미 처리된적 있는 노드라면 무시.
-        if (::distance[current_node] < dist )
+        if (distances[current_node] < dist )
         {
             continue;
         }
 
 		// 현재 노드와 연결된 다른 인접한 노드들을 확인
-		for (int n{}; n < graph[current_node].size(); ++n)
-		{
-			int cost = dist + graph[current_node][n].second;
-			// 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-			if (cost < ::distance[graph[current_node][n].first])
-			{
-				::distance[graph[current_node][n].first] = cost;
-                pq.emplace(make_pair(-cost, graph[current_node][n].first));
-			}
-		}
+        for ( auto& it : graph[current_node])
+        {
+            int adjacent_node = it.first;
+            int cost = dist + it.second; // 비용
+
+            // 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if (cost < distances[adjacent_node])
+            {
+                distances[adjacent_node] = cost; // 짧은거로 업데이트.
+                pq.emplace(make_pair(-cost, adjacent_node)); // 음수로 해야 max힙을 min힙처럼 사용 가능 쌉가능.
+            }
+        }
     }
 }
 
@@ -55,7 +57,7 @@ int solution()
     cin >> start_node;
     
     //// 최단 거리 테이블 초기화
-    fill_n(::distance, 100'001, inf);
+    fill_n(::distances, 100'001, inf);
 
     // 간선 정보 입력
     for (int n{}; n < edge_num; ++n)
@@ -71,14 +73,14 @@ int solution()
     for (int n{1}; n <= node_num; ++n)
     {
         // 도달 할수 없는 경우, 무한 출력
-        if (::distance[n] == inf)
+        if (::distances[n] == inf)
         {
             cout << "INFINITY" << '\n';
         }
         // 도달할 수 있는 경우 거리를 출력
         else
         {
-            cout << ::distance[n] << '\n';
+            cout << ::distances[n] << '\n';
         }
     }
 
