a0c67654ad24e5723c96f0c5e730cd6dfb57ee40
charg2dang <55304275+charg2dang@users.noreply.github.com>
2020-10-19 15:25:23 +0900

    using trie.

---
+++ baekjoon/14426.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class trie
+{
+	struct node_t
+	{
+		node_t() : c{}, nexts{}
+		{}
+
+		char c;
+		node_t* nexts[26];
+	};
+
+public:
+	trie() : root{ new node_t() }
+	{}
+
+	void push(const string& word)
+	{
+		node_t* cur = root;
+
+		for (char c : word)
+		{
+			node_t* next = cur->nexts[c - 'a'];
+
+			if (next != nullptr)
+			{
+				cur = next;
+			}
+			else
+			{
+				node_t* new_node = new node_t();
+				new_node->c = c;
+
+				cur->nexts[c - 'a'] = new_node;
+
+				cur = cur->nexts[c - 'a'];
+			}
+		}
+	}
+
+	bool contain(const string& word)
+	{
+		node_t* cur = root;
+
+		for (char c : word)
+		{
+			node_t* next = cur->nexts[c - 'a'];
+			if (next != nullptr)
+			{
+				cur = next;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+private:
+	node_t* root;
+};
+
+
+
+int main()
+{
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	long long  base_cnt, test_cnt;
+	cin >> base_cnt >> test_cnt;
+
+	trie t;
+
+	string str;
+	for (int n{ 0 }; n < base_cnt; ++n)
+	{
+		str.clear();
+
+		cin >> str;
+
+		t.push(str);
+	}
+
+	int counter{};
+	for (int n{ 0 }; n < test_cnt; ++n)
+	{
+		str.clear();
+
+		cin >> str;
+
+		if (t.contain(str) == true)
+		{
+			counter += 1;
+		}
+	}
+
+	cout << counter;
+	
+	return 0;
+}
